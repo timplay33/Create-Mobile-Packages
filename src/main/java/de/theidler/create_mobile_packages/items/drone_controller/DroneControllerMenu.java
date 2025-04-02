3df345bcb58fc08b796a59cef57c8b4a48ab9cfc
Tim <tim@heidler.eu.org>
2025-04-02 10:17:16 +0200

    :sparkles: Add #6 saving of the last Package Address

--- src/main/java/de/theidler/create_mobile_packages/items/drone_controller/DroneControllerMenu.java
+++ src/main/java/de/theidler/create_mobile_packages/items/drone_controller/DroneControllerMenu.java
@@ -11,14 +11,12 @@ import java.util.List;
 
 public class DroneControllerMenu extends AbstractContainerMenu {
     private final List<BigItemStack> bigItemStacks;
-    public String previouslyUsedAddress;
     public DroneController droneController;
 
     public DroneControllerMenu(int id, Inventory playerInventory, List<BigItemStack> inventorySummary, DroneController droneController) {
         super(CMPMenuTypes.DRONE_CONTROLLER_MENU.get(), id);
         this.bigItemStacks = inventorySummary;
         this.droneController = droneController;
-        previouslyUsedAddress = "";
     }
 
     public List<BigItemStack> getBigItemStacks() {
