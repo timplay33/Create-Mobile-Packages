ab92164bc4a2fcd42cf1dea2ab0a14e000ac1f46
Tim Heidler <tim@heidler.eu.org>
2025-04-18 18:12:18 +0200

    Fix Refactor DroneEntity to RoboBeeEntity #33

--- src/main/java/de/theidler/create_mobile_packages/CreateMobilePackagesClient.java
+++ src/main/java/de/theidler/create_mobile_packages/CreateMobilePackagesClient.java
@@ -1,6 +1,6 @@
 package de.theidler.create_mobile_packages;
 
-import de.theidler.create_mobile_packages.entities.models.DroneEntityModel;
+import de.theidler.create_mobile_packages.entities.models.RoboBeeModel;
 import de.theidler.create_mobile_packages.entities.render.DroneEntityRenderer;
 import de.theidler.create_mobile_packages.index.CMPEntities;
 import de.theidler.create_mobile_packages.index.ponder.CMPPonderPlugin;
@@ -25,9 +25,9 @@ public class CreateMobilePackagesClient {
     }
 
     public static void registerEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-        event.registerEntityRenderer(CMPEntities.DRONE_ENTITY.get(), DroneEntityRenderer::new);
+        event.registerEntityRenderer(CMPEntities.ROBO_BEE_ENTITY.get(), DroneEntityRenderer::new);
     }
     public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
-        event.registerLayerDefinition(DroneEntityModel.LAYER_LOCATION, DroneEntityModel::createBodyLayer);
+        event.registerLayerDefinition(RoboBeeModel.LAYER_LOCATION, RoboBeeModel::createBodyLayer);
     }
 }
