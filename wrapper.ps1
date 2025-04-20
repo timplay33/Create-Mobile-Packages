4e069bc3381009b3e50fa2690b1f837b1a39ac73
Tim Heidler <info@theidler.de>
2025-04-20 20:59:39 +0200

    update wrapper comments #42

--- wrapper.ps1
+++ wrapper.ps1
@@ -1,7 +1,9 @@
 <#
 .SYNOPSIS
-  Git-Flow Wrapper für PowerShell (Windows)
+  Git-Flow Wrapper for PowerShell (Windows)
   Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned
+  
+  usage:
   ..\wrapper.ps1 1.21.1 hotfix start "#issue"
 #>
 
@@ -10,7 +12,7 @@ param(
   [Parameter(ValueFromRemainingArguments=$true)][string[]]$FlowArgs
 )
 
-# Setze Git-Flow-Konfiguration
+# set git flow config
 git config gitflow.branch.master "mc$Version/main"
 git config gitflow.branch.develop "mc$Version/dev"
 git config gitflow.prefix.feature "mc$Version/feature/"
@@ -18,5 +20,5 @@ git config gitflow.prefix.hotfix   "mc$Version/hotfix/"
 git config gitflow.prefix.release  "mc$Version/release/"
 git config gitflow.prefix.support  "mc$Version/support/"
 
-# Führe git flow aus
+# run git flow
 git flow @FlowArgs
