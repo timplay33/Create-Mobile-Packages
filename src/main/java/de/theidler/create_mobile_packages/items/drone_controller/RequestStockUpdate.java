be826d696b5fa5ede3293d11a7a7dbb6b8402598
Tim <tim@heidler.eu.org>
2025-04-28 20:08:46 +0200

    Fix server Side error #42

--- src/main/java/de/theidler/create_mobile_packages/items/drone_controller/RequestStockUpdate.java
+++ src/main/java/de/theidler/create_mobile_packages/items/drone_controller/RequestStockUpdate.java
@@ -1,5 +1,6 @@
 package de.theidler.create_mobile_packages.items.drone_controller;
 
+import com.simibubi.create.content.logistics.packagerLink.LogisticsManager;
 import de.theidler.create_mobile_packages.index.CMPPackets;
 import net.createmod.catnip.net.base.ServerboundPacketPayload;
 import net.createmod.catnip.platform.CatnipServices;
@@ -10,7 +11,7 @@ import net.minecraft.server.level.ServerPlayer;
 
 import java.util.UUID;
 
-import static de.theidler.create_mobile_packages.items.drone_controller.StockCheckingItem.getAccurateSummary;
+import static de.theidler.create_mobile_packages.items.drone_controller.StockCheckingItem.networkFromStack;
 
 public class RequestStockUpdate implements ServerboundPacketPayload {
     public static final StreamCodec<RegistryFriendlyByteBuf, RequestStockUpdate> STREAM_CODEC = StreamCodec.composite(
@@ -33,7 +34,11 @@ public class RequestStockUpdate implements ServerboundPacketPayload {
     @Override
     public void handle(ServerPlayer player) {
         if (player != null) {
-            BigItemStackListPacket responsePacket = new BigItemStackListPacket(getAccurateSummary(player.getMainHandItem()).getStacks());
+            UUID Freq = networkFromStack(player.getMainHandItem());
+            if (Freq == null) {
+                return;
+            }
+            BigItemStackListPacket responsePacket = new BigItemStackListPacket(LogisticsManager.getSummaryOfNetwork(Freq, true).getStacks());
             CatnipServices.NETWORK.sendToClient(player, responsePacket);
         }
     }
