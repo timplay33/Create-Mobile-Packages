7c677453c2229e2f168958943da73cf84bbe97f9
Tim <tim@heidler.eu.org>
2025-04-30 15:34:54 +0200

    Add basic Mobile Packager GUI integration #47

--- src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerMenu.java
+++ src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerMenu.java
@@ -3,12 +3,18 @@ package de.theidler.create_mobile_packages.items.mobile_packager;
 import com.simibubi.create.foundation.gui.menu.MenuBase;
 import de.theidler.create_mobile_packages.index.CMPMenuTypes;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class MobilePackagerMenu extends MenuBase<MobilePackager> {
+
+    public ItemStackHandler proxyInventory;
+
     public MobilePackagerMenu(int id, Inventory inv, FriendlyByteBuf extraData) {
         super(CMPMenuTypes.MOBILE_PACKAGER_MENU.get(), id, inv, extraData);
     }
@@ -24,12 +30,13 @@ public class MobilePackagerMenu extends MenuBase<MobilePackager> {
 
     @Override
     public void initAndReadInventory(MobilePackager contentHolder) {
-
+        proxyInventory = new ItemStackHandler(1);
     }
 
     @Override
     public void addSlots() {
-
+        addSlot(new SlotItemHandler(proxyInventory, 0, 16, 24));
+        addPlayerSlots(18, 106);
     }
 
     @Override
@@ -38,7 +45,32 @@ public class MobilePackagerMenu extends MenuBase<MobilePackager> {
     }
 
     @Override
-    public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
-        return null;
+    public ItemStack quickMoveStack(Player pPlayer, int index) {
+        Slot clickedSlot = getSlot(index);
+        if (!clickedSlot.hasItem())
+            return ItemStack.EMPTY;
+
+        ItemStack stack = clickedSlot.getItem();
+        int size = 1;
+        boolean success = false;
+        if (index < size) {
+            success = !moveItemStackTo(stack, size, slots.size(), false);
+        } else
+            success = !moveItemStackTo(stack, 0, size, false);
+
+        return success ? ItemStack.EMPTY : stack;
+    }
+
+    @Override
+    public void removed(Player playerIn) {
+        ItemStack stack = proxyInventory.getStackInSlot(0);
+        if (stack.isEmpty()) {
+            super.removed(playerIn);
+            return;
+        }
+
+        if (!playerIn.getInventory().add(stack))
+            playerIn.level().addFreshEntity(new ItemEntity(playerIn.level(), playerIn.getX(), playerIn.getY(), playerIn.getZ(), stack));
+        super.removed(playerIn);
     }
 }
--- src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerScreen.java
+++ src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerScreen.java
@@ -4,16 +4,17 @@ import com.simibubi.create.foundation.gui.AllGuiTextures;
 import com.simibubi.create.foundation.gui.ScreenWithStencils;
 import com.simibubi.create.foundation.gui.menu.AbstractSimiContainerScreen;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.network.chat.Component;
+import net.minecraft.util.FormattedCharSequence;
 import net.minecraft.world.entity.player.Inventory;
 
 public class MobilePackagerScreen extends AbstractSimiContainerScreen<MobilePackagerMenu>
         implements ScreenWithStencils {
 
-    private static final int CARD_HEADER = 20;
-    private static final int CARD_WIDTH = 160;
-
     final int slices = 4;
+
+    private EditBox editorEditBox;
 
     public MobilePackagerScreen(MobilePackagerMenu container, Inventory inv, Component title) {
         super(container, inv, title);
@@ -29,7 +30,18 @@ public class MobilePackagerScreen extends AbstractSimiContainerScreen<MobilePack
     }
 
     @Override
-    protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
+    protected void renderBg(GuiGraphics graphics, float pPartialTick, int pMouseX, int pMouseY) {
+        int y = topPos - 5;
+        AllGuiTextures.STOCK_KEEPER_CATEGORY_HEADER.render(graphics, leftPos, y);
+        y += AllGuiTextures.STOCK_KEEPER_CATEGORY_HEADER.getHeight();
+        AllGuiTextures.STOCK_KEEPER_CATEGORY_EDIT.render(graphics, leftPos, y);
+        y += AllGuiTextures.STOCK_KEEPER_CATEGORY_EDIT.getHeight();
+        AllGuiTextures.STOCK_KEEPER_CATEGORY_FOOTER.render(graphics, leftPos, y);
 
+        renderPlayerInventory(graphics, leftPos + 10, topPos + 88);
+
+        FormattedCharSequence formattedcharsequence = Component.translatable("item.create_mobile_packages.mobile_packager")
+                .getVisualOrderText();
+        graphics.drawString(font, formattedcharsequence, AllGuiTextures.STOCK_KEEPER_CATEGORY_EDIT.getWidth() - font.width(formattedcharsequence)/4, topPos - 1, 0x3D3C48, false);
     }
 }
