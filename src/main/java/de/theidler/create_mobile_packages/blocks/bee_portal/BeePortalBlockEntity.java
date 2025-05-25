fa3925f6e6db86b20bdc8bfd4c951a8a48f820b2
Stonygeist123 <yunus.ibrahim.hassen@gmail.com>
2025-05-25 12:07:48 +0200

    Added: - Clean.
    Long-term ToDos:
    - Recipe
    - Texture
    - Calc ETA
    - Dimension option in stock ticker GUI (e.g. if multiple BeePorts with same name exist)
    - Multi-dimensional range check (for better code structure; not essential)
    - Fix dimension teleportation for multiplayer (not sure if it does not work)
    - Fix queue for BeePortal

--- src/main/java/de/theidler/create_mobile_packages/blocks/bee_portal/BeePortalBlockEntity.java
+++ src/main/java/de/theidler/create_mobile_packages/blocks/bee_portal/BeePortalBlockEntity.java
@@ -1,28 +1,20 @@
 package de.theidler.create_mobile_packages.blocks.bee_portal;
 
 import de.theidler.create_mobile_packages.blocks.bee_port.BeePortBlockEntity;
-import de.theidler.create_mobile_packages.entities.RoboBeeEntity;
 import de.theidler.create_mobile_packages.entities.robo_entity.Location;
 import de.theidler.create_mobile_packages.entities.robo_entity.RoboEntity;
-import de.theidler.create_mobile_packages.entities.robo_entity.states.AdjustRotationToTarget;
 import de.theidler.create_mobile_packages.index.CMPItems;
 import de.theidler.create_mobile_packages.index.CMPPackets;
 import net.minecraft.core.BlockPos;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.inventory.ContainerData;
-import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.items.ItemStackHandler;
-
-import java.util.Comparator;
-import java.util.concurrent.atomic.AtomicReference;
 
 import static de.theidler.create_mobile_packages.blocks.bee_portal.BeePortalBlock.IS_OPEN_TEXTURE;
 import static de.theidler.create_mobile_packages.index.CMPBlockEntities.beePortStorage;
@@ -32,8 +24,6 @@ import static de.theidler.create_mobile_packages.index.CMPBlockEntities.beePortS
  * to players or other drone ports using drones.
  */
 public class BeePortalBlockEntity extends BlockEntity {
-    private RoboEntity entityOnTravel = null;
-    private final ContainerData data = new SimpleContainerData(2);
     private final ItemStackHandler roboBeeInventory = new ItemStackHandler(1);
 
     /**
@@ -45,19 +35,6 @@ public class BeePortalBlockEntity extends BlockEntity {
      */
     public BeePortalBlockEntity(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
         super(pType, pPos, pBlockState);
-    }
-
-    private static void requestRoboEntity(Location location) {
-        AtomicReference<BeePortalBlockEntity> result = new AtomicReference<>();
-        beePortStorage.getAllBeePortals().forEach((currentLevel, allBEs) -> {
-            allBEs.removeIf(be -> currentLevel == location.dimensionType());
-            allBEs.removeIf(be -> be.getRoboBeeInventory().getStackInSlot(0).getCount() <= 0);
-            result.set(allBEs.stream()
-                    .min(Comparator.comparingDouble(a -> a.getBlockPos().distSqr(location.position())))
-                    .orElse(null));
-        });
-
-//        if (result.get() != null) result.get().sendDrone(location);
     }
 
     /**
@@ -74,26 +51,16 @@ public class BeePortalBlockEntity extends BlockEntity {
                 SoundSource.BLOCKS);
     }
 
-    public void sendDrone(ItemStack itemStack) {
-        if (roboBeeInventory.getStackInSlot(0).getCount() <= 0) {
-//            if (this.entityOnTravel == null) requestRoboEntity(this.location());
-            return;
-        }
-
-        RoboBeeEntity drone = new RoboBeeEntity(level, itemStack, null, this.getBlockPos());
-        level.addFreshEntity(drone);
-        roboBeeInventory.getStackInSlot(0).shrink(1);
-    }
-
     public void sendDrone(RoboEntity re) {
         if (roboBeeInventory.getStackInSlot(0).getCount() <= 0) {
             return;
         }
 
-        BeePortalBlockEntity targetPortal = re.getTargetPortalEntity();
         BeePortBlockEntity targetBlock = re.getTargetBlockEntity();
+        Level targetLevel = targetBlock.getLevel();
+        if (targetLevel == null) return;
         CMPPackets.getChannel()
-                .sendToServer(new RequestDimensionTeleport(targetBlock.getLevel().dimension().location(), getBlockPos().getCenter(), targetBlock.getBlockPos(), re.getItemStack()));
+                .sendToServer(new RequestDimensionTeleport(targetLevel.dimension().location(), getBlockPos().getCenter(), targetBlock.getBlockPos(), re.getItemStack()));
     }
 
     /**
@@ -102,17 +69,15 @@ public class BeePortalBlockEntity extends BlockEntity {
     @Override
     public void onLoad() {
         super.onLoad();
+        if (level == null) return;
         beePortStorage.addBeePortal(this, level.dimensionType());
     }
 
     @Override
     public void setRemoved() {
+        if (level == null) return;
         if (!level.isClientSide) {
             beePortStorage.removeBeePortal(this, level.dimensionType());
-            if (entityOnTravel != null) {
-                entityOnTravel.setTargetVelocity(Vec3.ZERO);
-                entityOnTravel.setState(new AdjustRotationToTarget());
-            }
             if (roboBeeInventory.getStackInSlot(0).getCount() > 0) {
                 level.addFreshEntity(new ItemEntity(level, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), roboBeeInventory.getStackInSlot(0)));
             }
@@ -120,31 +85,12 @@ public class BeePortalBlockEntity extends BlockEntity {
         super.setRemoved();
     }
 
-    public synchronized boolean trySetEntityOnTravel(RoboEntity entity) {
-        if (entityOnTravel == null || entity == null) {
-            entityOnTravel = entity;
-            return true;
-        }
-        return false;
-    }
-
-    public ItemStackHandler getRoboBeeInventory() {
-        return roboBeeInventory;
-    }
-
     public void addBeeToRoboBeeInventory(int amount) {
         roboBeeInventory.insertItem(0, new ItemStack(CMPItems.ROBO_BEE.get(), amount), false);
     }
 
-    public RoboEntity getRoboEntity() {
-        return entityOnTravel;
-    }
-
-    public ContainerData getData() {
-        return data;
-    }
-
     public Location location() {
-        return new Location(getBlockPos(), getLevel().dimensionType());
+        Level level = getLevel();
+        return level == null ? new Location(getBlockPos(), null) : new Location(getBlockPos(), level.dimensionType());
     }
 }
