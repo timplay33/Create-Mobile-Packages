49dd1c27b9c447016bb7e5127eed3f69f2af1d89
Tim <info@theidler.de>
2025-06-24 14:38:31 +0200

    Fix Creative Tab error by removing any Tab related Code → Deprecated Items are now visible
    Signed-off-by: Tim <info@theidler.de>

--- src/main/java/de/theidler/create_mobile_packages/index/CMPBlocks.java
+++ src/main/java/de/theidler/create_mobile_packages/index/CMPBlocks.java
@@ -40,8 +40,6 @@ public class CMPBlocks {
             .initialProperties(SharedProperties::wooden)
             .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
             .item(DronePortItem::new)
-            .removeTab(CreativeModeTabs.SEARCH)
-            .removeTab(ResourceKey.create(net.minecraft.core.registries.Registries.CREATIVE_MODE_TAB, CreateMobilePackages.asResource("create_mobile_packages_tab")))
             .transform(customItemModel())
             .register();
 
