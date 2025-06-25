c81c536f36eab38e569d650a481b65f9978b93da
Tim <tim@heidler.eu.org>
2025-06-25 20:18:51 +0200

    Fix PST launch error

--- src/main/java/de/theidler/create_mobile_packages/items/portable_stock_ticker/PortableStockTickerMenu.java
+++ src/main/java/de/theidler/create_mobile_packages/items/portable_stock_ticker/PortableStockTickerMenu.java
@@ -7,7 +7,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 
-import static com.simibubi.create.content.logistics.packagerLink.LogisticallyLinkedBlockItem.isTuned;
+import static de.theidler.create_mobile_packages.items.portable_stock_ticker.LogisticallyLinkedItem.isTuned;
 
 public class PortableStockTickerMenu extends AbstractContainerMenu {
     public PortableStockTicker portableStockTicker;
@@ -17,12 +17,12 @@ public class PortableStockTickerMenu extends AbstractContainerMenu {
     public PortableStockTickerMenu(int id, Inventory playerInventory) {
         super(CMPMenuTypes.PORTABLE_STOCK_TICKER_MENU.get(), id);
         ItemStack stack = PortableStockTicker.find(playerInventory);
-        if (stack != null && stack.getItem() instanceof PortableStockTicker pst)
-            if (isTuned(stack)) {
-                this.portableStockTicker = pst;
-            } else {
+        if (stack != null && stack.getItem() instanceof PortableStockTicker pst) {
+            if (!isTuned(stack)) {
                 playerInventory.player.displayClientMessage(Component.translatable("item.create_mobile_packages.portable_stock_ticker.not_linked"), true);
             }
+            this.portableStockTicker = pst;
+        }
         this.player = playerInventory.player;
     }
 
