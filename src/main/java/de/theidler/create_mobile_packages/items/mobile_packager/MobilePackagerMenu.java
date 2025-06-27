823c05fb3267ce297b18d6dca5779aa95547ffbf
Tim Heidler <info@theidler.de>
2025-06-27 19:29:46 +0200

    Fix missing import

--- src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerMenu.java
+++ src/main/java/de/theidler/create_mobile_packages/items/mobile_packager/MobilePackagerMenu.java
@@ -11,6 +11,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.ItemStackHandler;
