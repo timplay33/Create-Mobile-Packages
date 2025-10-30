cdc77304fff3decf17cb46cab493363eb3ef8e67
Tim <tim@heidler.eu.org>
2025-10-30 13:27:23 +0100

    Fix Porting Curios Support to 1.21.1 (#90)

--- src/main/java/de/theidler/create_mobile_packages/compat/curios/Curios.java
+++ src/main/java/de/theidler/create_mobile_packages/compat/curios/Curios.java
@@ -3,28 +3,18 @@ package de.theidler.create_mobile_packages.compat.curios;
 import de.theidler.create_mobile_packages.index.CMPItems;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import top.theillusivec4.curios.api.CuriosCapability;
-import top.theillusivec4.curios.api.SlotTypeMessage;
-import top.theillusivec4.curios.api.SlotTypePreset;
+import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 import top.theillusivec4.curios.api.type.inventory.ICurioStacksHandler;
 
 import java.util.Map;
 import java.util.Optional;
 
-// from package com.simibubi.create.compat.curios.Curios
 public class Curios {
 
     private static Optional<Map<String, ICurioStacksHandler>> resolveCuriosMap(LivingEntity entity) {
-        return entity.getCapability(CuriosCapability.INVENTORY).map(ICuriosItemHandler::getCurios);
-    }
-
-    public static void init(IEventBus modEventBus) {
-        modEventBus.addListener(Curios::onInterModEnqueue);
-        //modEventBus.addListener(onClientSetup);
+        return CuriosApi.getCuriosInventory(entity)
+                .map(ICuriosItemHandler::getCurios);
     }
 
     public static ItemStack findPortableStockTickerCurios(LivingEntity entity) {
@@ -33,18 +23,13 @@ public class Curios {
                 // Search all the curio slots for PST existing
                 int slots = stacksHandler.getSlots();
                 for (int slot = 0; slot < slots; slot++) {
-                    if (CMPItems.PORTABLE_STOCK_TICKER.isIn(stacksHandler.getStacks().getStackInSlot(slot))) {
-                        return stacksHandler.getStacks().getStackInSlot(slot);
+                    ItemStack stack = stacksHandler.getStacks().getStackInSlot(slot);
+                    if (CMPItems.PORTABLE_STOCK_TICKER.isIn(stack)) {
+                        return stack;
                     }
                 }
             }
             return ItemStack.EMPTY;
         }).orElse(ItemStack.EMPTY);
     }
-
-    private static void onInterModEnqueue(final InterModEnqueueEvent event) {
-        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.BODY.getMessageBuilder()
-                .build());
-    }
-
 }
