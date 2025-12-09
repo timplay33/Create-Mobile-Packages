22e6bdecf91a2f92a85958d2772e32b745739aa2
Tim <tim@heidler.eu.org>
2025-12-09 17:10:09 +0100

    Disable Entity Dimension Travel for RoboEntity
    Fixes #250

--- src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
+++ src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
@@ -1,8 +1,10 @@
 package de.theidler.create_mobile_packages;
 
+import de.theidler.create_mobile_packages.entities.robo_entity.RoboEntity;
 import de.theidler.create_mobile_packages.robo.RoboManager;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityTravelToDimensionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.LogicalSide;
 import net.minecraftforge.fml.common.Mod;
@@ -17,4 +19,13 @@ public class CommonEvents {
         if (!(event.level instanceof net.minecraft.server.level.ServerLevel)) return;
         RoboManager.get((ServerLevel) event.level).tick((ServerLevel) event.level);
     }
+
+    @SubscribeEvent
+    public static void onEntityTravelToDimension(EntityTravelToDimensionEvent event) {
+        if (event.getEntity().level().isClientSide()) return;
+        if (!(event.getEntity() instanceof RoboEntity)) return;
+
+        event.setCanceled(true);
+    }
+
 }
