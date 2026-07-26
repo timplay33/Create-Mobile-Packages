66308f6c37794267275dc1e23602aa1e0aed6ff7
Tim Heidler <info@theidler.de>
2026-07-26 13:22:20 +0200

    Add a PlayerNameCache class that caches player names with UUIDs and has utility methods for:
    - finding player name by UUID
    - finding player UUID by name
    - getting ServerPlayer by UUID
    - checking if player is online by UUID
    - matching player names to addresses

--- src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
+++ src/main/java/de/theidler/create_mobile_packages/CommonEvents.java
@@ -2,12 +2,16 @@ package de.theidler.create_mobile_packages;
 
 import de.theidler.create_mobile_packages.blocks.bee_port.BeePortBlockEntity;
 import de.theidler.create_mobile_packages.entities.robo_entity.RoboEntity;
+import de.theidler.create_mobile_packages.robo.PlayerNameCache;
 import de.theidler.create_mobile_packages.robo.RoboManager;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.EntityTravelToDimensionEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.tick.LevelTickEvent;
 
 
@@ -37,6 +41,15 @@ public class CommonEvents {
         if (!(event.getEntity() instanceof RoboEntity)) return;
 
         event.setCanceled(true);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getEntity();
+        if (player instanceof ServerPlayer serverPlayer) {
+            PlayerNameCache.get(serverPlayer.serverLevel())
+                    .addPlayerName(serverPlayer.getUUID(), serverPlayer.getGameProfile().getName());
+        }
     }
 
 }
